2afd707d59dce3976843af7fc765685326bf69b2
Changyuan Lin <coreballium@outlook.com>
2020-04-02 00:49:23 -0600

    Basic app bar and theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Typography, Toolbar, createMuiTheme, ThemeProvider, Container } from '@material-ui/core';
+import { red, blue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: red,
+    secondary: blue,
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              SCHEDULESTACKER JS
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="sm">
+          hello world?
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
