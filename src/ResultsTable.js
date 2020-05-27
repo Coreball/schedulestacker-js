60a08a232188e23a80c21c1471d1f7b6afadfa6d
Changyuan Lin <coreballium@outlook.com>
2020-05-26 20:03:46 -0600

    Expanding rows

--- src/ResultsTable.js
+++ src/ResultsTable.js
@@ -1,33 +1,117 @@
 import React from 'react';
-import { Typography, Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper, makeStyles } from '@material-ui/core';
+import { Typography, Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper, Collapse, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   tableContainer: {
     marginBottom: theme.spacing(2),
   },
-  headerCell: {
+  periodHeaderCell: {
     minWidth: 100,
   },
+  noBorderBottom: {
+    borderBottom: "unset",
+  },
+  collapsableCell: {
+    paddingBottom: 0,
+    paddingTop: 0,
+  },
+  detailTable: {
+    marginBottom: theme.spacing(1),
+  },
 }));
+
+const basicCellRep = (period) => {
+  switch (period.length) {
+    case 0:
+      return "---"
+    case 1:
+      return period[0].name
+    case 2:
+      const s1 = (period[0] && period[0].name) || "---";
+      const s2 = (period[1] && period[1].name) || "---";
+      return s1 + ", " + s2;
+    default:
+      return "Invalid length of period array!"
+  }
+};
+
+const expandedCellRep = (course) => {
+  if (course === null) {
+    return "---"
+  } else {
+    return course.name + " - " + course.teacher + " - " + course.room
+  }
+};
+
+function ExpandedRow(props) {
+  const expandPeriod = (periodCourses) => {
+    let s1 = "---", s2 = "---";
+    if (periodCourses.length === 1) {
+      s1 = expandedCellRep(periodCourses[0]);
+      s2 = s1;
+    } else if (periodCourses.length === 2) {
+      s1 = expandedCellRep(periodCourses[0]);
+      s2 = expandedCellRep(periodCourses[1]);
+    }
+    return (
+      <React.Fragment>
+        <TableCell>{s1}</TableCell>
+        <TableCell>{s2}</TableCell>
+      </React.Fragment>
+    )
+  };
+
+  return (
+    <TableRow>
+      <TableCell variant="head">Period {props.periodNum + 1}</TableCell>
+      {expandPeriod(props.periodCourses)}
+    </TableRow>
+  )
+}
+
+function ResultsRow(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow onClick={() => setOpen(!open)}>
+        {props.schedule.map((period) => (
+          <TableCell className={classes.noBorderBottom}>
+            {basicCellRep(period)}
+          </TableCell>
+        ))}
+      </TableRow>
+      <TableRow>
+        <TableCell className={classes.collapsableCell} colSpan={8}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Typography variant="h6" gutterBottom>
+              Details
+            </Typography>
+            <Table className={classes.detailTable} size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Schedule {props.number}</TableCell>
+                  <TableCell>Semester 1</TableCell>
+                  <TableCell>Semester 2</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {props.schedule.map((periodCourses, periodNum) => (
+                  <ExpandedRow periodCourses={periodCourses} periodNum={periodNum} />
+                ))}
+              </TableBody>
+            </Table>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  )
+}
 
 export default function ResultsTable(props) {
   const classes = useStyles();
   const headers = Array(8).fill(0).map((v, i) => "Period " + (i + 1));
-
-  const cellRep = (period) => {
-    switch (period.length) {
-      case 0:
-        return "-"
-      case 1:
-        return period[0].name
-      case 2:
-        const s1 = (period[0] && period[0].name) || "-";
-        const s2 = (period[1] && period[1].name) || "-";
-        return s1 + ", " + s2;
-      default:
-        return "Invalid length of period array!"
-    }
-  };
 
   return (
     <div className="ResultsTable">
@@ -39,19 +123,15 @@ export default function ResultsTable(props) {
           <TableHead>
             <TableRow>
               {headers.map((heading) => (
-                <TableCell key={heading} className={classes.headerCell}>{heading}</TableCell>
+                <TableCell key={heading} className={classes.periodHeaderCell}>
+                  {heading}
+                </TableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.schedules.map((schedule) => (
-              <TableRow>
-                {schedule.map((period) => (
-                  <TableCell>
-                    {cellRep(period)}
-                  </TableCell>
-                ))}
-              </TableRow>
+            {props.schedules.map((schedule, index) => (
+              <ResultsRow schedule={schedule} number={index + 1} />
             ))}
           </TableBody>
         </Table>
